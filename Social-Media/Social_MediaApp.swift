e30983dccb7c8d68c1c9d9bf287723c02becfa0e
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2023-01-12 18:15:35 +0200

    Initial Commit

---
+++ Social-Media/Social_MediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_MediaApp.swift
+//  Social-Media
+//
+//  Created by KhaleD HuSsien on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Social_MediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
